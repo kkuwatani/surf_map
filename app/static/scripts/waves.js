dfd84827b23e6ceabf7552f0f142eb09ff2eb2a9
Kyle Kuwatani <kkuwatani@tesla.com>
2017-08-13 15:06:26 -0700

    start of wave animations

--- app/static/scripts/waves.js
+++ app/static/scripts/waves.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+    function wave_animation_loop($wave, start_x, start_y, end_x, end_y, delay) {
+        $wave
+            .delay(delay)
+            .animate({'left': end_x,
+                       'top': end_y},
+                       5000,
+                       'linear')
+             .animate({'left': start_x,
+                       'top': start_y},
+                        0,
+                        'linear');
+        window.setTimeout(function() { wave_animation_loop($wave, start_x, start_y, end_x, end_y, delay) }, 5000);
+    }
+
     wave_space = $('#wrapper');
 
     $.ajax({
@@ -9,10 +23,22 @@ $(document).ready(function(){
 //            console.log(data);
             $.each(data.Swell, function(i, wave) {
                 console.log(wave);
-                var $div = $("<div>", {id: "testtesttest"});
-                $div.html("hello world");
+                var $div = $("<div>", {id: i});
+                $div.css({
+                    'background-color': '#FF0000',
+                    'height': '20px',
+                    'width': '20px',
+                    'position' : 'absolute',
+                    'top': 0,
+                    'left': 0
+                });
+                $div.html(i);
                 wave_space.append($div);
+                wave_animation_loop($div, 0, 0, 500, 500, 2000);
             })
         }
     })
+
+
+
 });
